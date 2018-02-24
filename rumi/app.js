cd987fd81af5834b2ab467c5269d29f04b920dad
Joshua Sylvestre <joshuasylvestre@knights.ucf.edu>
2018-02-24 12:12:24 -0500

    Switched to HTML

--- rumi/app.js
+++ rumi/app.js
@@ -25,8 +25,9 @@ var db = monk(uri);
 //1234
 
 // view engine setup
+app.engine('html', require('ejs').renderFile);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -53,13 +54,16 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  console.error(err.stack);
+	
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error.html');
+
 });
 
 module.exports = app;
