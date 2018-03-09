c92bdbbc81be5fd72e43593b26006c9b2dac0979
stevedang125 <stevedang125@gmail.com>
2018-03-09 06:44:36 -0500

    temp. look for the dashboard

--- rumi/public/js/dashboard.js
+++ rumi/public/js/dashboard.js
@@ -1,6 +1,12 @@
 var app = angular.module("RumiDashboard", ['ngRoute', 'dashboardRoutes']);
 
 app.controller('DashboardController', function($scope) {
+    $scope.date = "12/23/2945";
+    $scope.total = "$65";
+    $scope.lastname = "Potter";
+    $scope.firstname = "Hairy";
+    $scope.type = "Rent";
+    $scope.type2 = "New Friend";
 
 });
 
