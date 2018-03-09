01200e2310c311ca17143d07664b7b8c0e8c84a1
Dita Dewindita <dita.dewindita@gmail.com>
2018-03-09 14:46:36 -0500

    Left out some merge conflicts lol

--- rumi/public/js/controllers/dashboard/main.js
+++ rumi/public/js/controllers/dashboard/main.js
@@ -1,8 +1,4 @@
 angular.module('RumiDashboard.controllers').controller('DashboardController', function($scope, UserService) {
-  $scope.$on('userAdded', function() {
-    $scope.user = UserService.getUser();
-  });
-
   $scope.date = "12/23/2945";
   $scope.total = "$65";
   $scope.lastname = "Potter";
